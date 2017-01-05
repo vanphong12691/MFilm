07d33b1885b6c422b66db5d5edf2a931bf0a63b0
PHONG <vanphong12691@gmail.com>
2017-01-05 15:50:46 +0900

    Add like

--- app/common/constants.js
+++ app/common/constants.js
@@ -12,6 +12,7 @@ exports.IMAGE_MESSAGE_WIDTH = 150;
 //define key store
 exports.SETTING_STORE_KEY = 'SETTING_STORE_KEY';
 exports.SEEN_STORE_KEY = 'SEEN_STORE_KEY';
+exports.LIKE_STORE_KEY = 'LIKE_STORE_KEY';
 
 //define route id
 exports.HOME_ID = 1;
